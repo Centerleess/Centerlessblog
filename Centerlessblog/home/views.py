2296b71dbd29c920d87e04ddb0cbd87d13320c3a
Center <guisha223@163.com>
2023-06-28 22:59:20 +0800

    新增前端展示博客数据、分页管理

--- Centerlessblog/home/views.py
+++ Centerlessblog/home/views.py
@@ -2,12 +2,12 @@
 # -*- coding: UTF-8 -*-
 import logging
 
-from django.http import HttpResponseBadRequest
+from django.core.paginator import Paginator, EmptyPage
+from django.http import HttpResponseBadRequest, HttpResponseNotFound
 from django.shortcuts import render
 from django.views import View
 
-from home.models import ArticleCategory
-
+from home.models import ArticleCategory, Article
 
 logger = logging.getLogger("django_log")
 # Create your views here.
@@ -29,9 +29,31 @@ class IndexView(View):
         except ArticleCategory.DoesNotExist as e:
             logger.error(e)
             return HttpResponseBadRequest("没有此分类")
+        # 获取分页参数
+        page_num = request.GET.get('page_num', 1)
+        page_size = request.GET.get('page_size', 10)
+        # 根据分类查数据
+        articles = Article.objects.filter(category=category)
+
+        # 创建分页器
+        paginator = Paginator(articles, page_size)
+        # 进行分页处理
+        try:
+            page_articles = paginator.page(page_num)
+        except EmptyPage:
+            # 如果没有分页数据，默认给用户404
+            return HttpResponseNotFound('empty page')
+
+        # 总页数
+        total_page = paginator.num_pages
         # 传递给模板
         context = {
             "categories": categories,
             "category": category,
+            # 分类数据
+            "articles": page_articles,
+            "page_size": page_size,
+            "page_num": page_num,
+            "total_page": total_page
         }
         return render(request, 'index.html', context=context)
