cd7c9c2d57ea680f47be9af355e6a04c836ee7d5
Center <guisha223@163.com>
2023-06-28 21:50:28 +0800

    博客首页分类展示

--- Centerlessblog/home/views.py
+++ Centerlessblog/home/views.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+import logging
+
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render
 from django.views import View
 
+from home.models import ArticleCategory
 
+
+logger = logging.getLogger("django_log")
 # Create your views here.
 class IndexView(View):
     """首页广告"""
     def get(self, request):
         """
-        重定向首页
+        页面展示
         :param request:
         :return:
         """
-        return render(request, 'index.html')
+        # 标签分类展示
+        categories = ArticleCategory.objects.all()
+        # 用户点击的分类ID
+        cat_id = request.GET.get('cat_id', 1)
+        # 判断id
+        try:
+            category = ArticleCategory.objects.get(id=cat_id)
+        except ArticleCategory.DoesNotExist as e:
+            logger.error(e)
+            return HttpResponseBadRequest("没有此分类")
+        # 传递给模板
+        context = {
+            "categories": categories,
+            "category": category,
+        }
+        return render(request, 'index.html', context=context)
