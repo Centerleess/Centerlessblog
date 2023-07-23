edbdec4bebf6c911401522e9d28da62120bccceb
Center <guisha223@163.com>
2023-07-23 19:10:04 +0800

    uswgi

--- Centerlessblog/Centerlessblog/settings.py
+++ Centerlessblog/Centerlessblog/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rhf+ehm8^9*vdsv00lb(xr2jmm8tgtx1bc9wdm0qdntuf(_&w*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['118.25.20.119']
 
 # Application definition
 
